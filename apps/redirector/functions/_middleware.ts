812945c8e602e1f723a8e02eb5d7a9490b1349d4
deniz <43997085+deniz-blue@users.noreply.github.com>
2026-03-06 19:55:06 +0200

    fuck yeah opengraph embeds

---
+++ apps/redirector/functions/_middleware.ts
@@ -0,0 +1,114 @@
+/// <reference types="@cloudflare/workers-types" />
+/// <reference types="@atcute/atproto" />
+
+import { Client, simpleFetchHandler } from "@atcute/client";
+import { CompositeDidDocumentResolver, CompositeHandleResolver, DohJsonHandleResolver, LocalActorResolver, PlcDidDocumentResolver, WebDidDocumentResolver, WellKnownHandleResolver } from "@atcute/identity-resolver";
+import { parseResourceUri } from "@atcute/lexicons";
+import { type EventData, EventDataSchema, type KnownEventComponent, type Translations } from "@evnt/schema";
+import { snippetToMarkdown } from "@evnt/pretty/markdown";
+import { snippetEvent } from "@evnt/pretty";
+
+interface Env { }
+
+const actorResolver = new LocalActorResolver({
+	handleResolver: new CompositeHandleResolver({
+		methods: {
+			dns: new DohJsonHandleResolver({ dohUrl: 'https://mozilla.cloudflare-dns.com/dns-query' }),
+			http: new WellKnownHandleResolver(),
+		},
+	}),
+	didDocumentResolver: new CompositeDidDocumentResolver({
+		methods: {
+			plc: new PlcDidDocumentResolver(),
+			web: new WebDidDocumentResolver(),
+		},
+	}),
+})
+
+// Throws errors, beware!
+const fetchEventData = async (source: string): Promise<EventData | null> => {
+	if (source.startsWith("at://")) {
+		const parsed = parseResourceUri(source);
+		if (!parsed.ok || !parsed.value.collection || !parsed.value.rkey) return null;
+
+		const { pds } = await actorResolver.resolve(parsed.value.repo);
+
+		const rpc = new Client({
+			handler: simpleFetchHandler({
+				service: pds,
+			}),
+		});
+
+		const res = await rpc.get("com.atproto.repo.getRecord", {
+			params: {
+				repo: parsed.value.repo,
+				collection: parsed.value.collection,
+				rkey: parsed.value.rkey,
+			},
+		});
+
+		if (!res.ok) return null;
+		return EventDataSchema.parse(res.data.value);
+	} else if (source.startsWith("http://") || source.startsWith("https://")) {
+		const res = await fetch(source);
+		if (!res.ok) return null;
+		const json = await res.json();
+		return EventDataSchema.parse(json);
+	} else return null;
+};
+
+export const onRequest: PagesFunction<Env> = async (ctx) => {
+	const url = new URL(ctx.request.url);
+	const response = await ctx.next();
+
+	let data: EventData | null = null;
+
+	try {
+		let source = url.searchParams.get("source") ?? url.searchParams.get("url");
+		if (
+			url.searchParams.get("action") === "view-event"
+			&& source
+		) {
+			console.log("Fetching event data for source:", source);
+			data = await fetchEventData(source);
+		}
+	} catch (err) {
+		console.error("Error fetching event data:", err);
+	}
+
+	const t = (translations: Translations): string =>
+		translations["en"] || translations[Object.keys(translations)[0]] || "";
+
+	let title = t(data?.name ?? {}) ?? null;
+
+	let markdown = data ? snippetEvent(data).map(snip => snippetToMarkdown(snip)).join("\n") : "";
+
+	let splashMediaComponents = data?.components
+		?.filter((c): c is KnownEventComponent & { type: "splashMedia" } => c.type === "splashMedia")
+		.map(c => c.data)
+	let selected = splashMediaComponents?.find(c => c.roles.includes("ogembed"))
+		?? splashMediaComponents?.find(c => c.roles.includes("embed"))
+		?? splashMediaComponents?.find(c => c.roles.includes("poster"))
+		?? splashMediaComponents?.[0];
+	let image = selected?.media.sources[0];
+
+	return new HTMLRewriter()
+		.on('head', {
+			element(element) {
+				if (data) {
+					element.append(`<meta property="twitter:card" content="summary_large_image"/>`, { html: true });
+					element.append(`<meta property="og:title" content="${title!}" />`, { html: true });
+					if (markdown) element.append(`<meta property="og:description" content="${markdown.slice(0, 200)}" />`, { html: true });
+					if (image) {
+						element.append(`<meta property="og:image" content="${image.url}" />`, { html: true });
+						element.append(`<meta property="twitter:image" content="${image.url}" />`, { html: true });
+						if (image.dimensions) {
+							element.append(`<meta property="og:image:width" content="${image.dimensions.width}" />`, { html: true });
+							element.append(`<meta property="og:image:height" content="${image.dimensions.height}" />`, { html: true });
+						}
+					}
+				}
+			},
+		})
+		.transform(response);
+};
--- packages/event-pretty/src/markdown/index.ts
+++ packages/event-pretty/src/markdown/index.ts
@@ -1,6 +1,64 @@
+import { UtilPartialDate } from "@evnt/schema/utils";
+import type { SnippetIcon, SnippetLabel, TSnippet } from "../core/snippet";
+
 export const getClockEmoji = (time: string) => {
 	const [hours, minutes] = time.split(":").map(Number);
 	let hour12 = (hours ?? 0) % 12 || 12;
 	const base = ((minutes ?? 0) >= 30) ? 0x1F55C : 0x1F550;
 	return String.fromCodePoint(base + hour12 - 1);
+};
+
+export const snippetLabelToMarkdown = (label: SnippetLabel): string => {
+	if (label.type === "text") return label.value;
+	if (label.type === "placeholder") switch (label.hint) {
+		case "unknown": return "Unknown";
+		case "unnamed": return "Unnamed";
+	}
+	if (label.type === "translations")
+		return label.value["en"] || label.value[Object.keys(label.value)[0]!] || "";
+	if (label.type === "external-link")
+		return label.name ? `[${label.name}](${label.url})` : label.url;
+	if (label.type === "address")
+		return (label.value.addr ?? "") + (label.value.countryCode ? (" " + (
+			String.fromCodePoint(...label.value.countryCode.toUpperCase()
+				.split("")
+				.map(char => 127397 + char.charCodeAt(0)))
+		)) : "");
+
+	if (label.type === "partial-date" || label.type === "date-time") return UtilPartialDate.toIntlString(label.value, {
+		timezone: "UTC",
+		locale: "en",
+		noCurrentYear: false,
+	}) + " (UTC)";
+
+	if (label.type === "time") return label.value;
+
+	if (label.type === "time-range") return `${label.value.start.value} - ${label.value.end.value}`;
+
+	if (label.type === "date-time-range") return `${UtilPartialDate.toIntlString(label.value.start, {
+		timezone: "UTC",
+		locale: "en",
+		noCurrentYear: false,
+	})} - ${UtilPartialDate.toIntlString(label.value.end, {
+		timezone: "UTC",
+		locale: "en",
+		noCurrentYear: false,
+	})} (UTC)`;
+
+	return "";
+};
+
+export const snippetToMarkdown = (snippet: TSnippet): string => {
+	let emoji = snippet.icon ? ({
+		clock: getClockEmoji(snippet.label?.type === "time" ? snippet.label.value : "00:00"),
+		calendar: "📅",
+		"venue-online": "🌐",
+		"venue-physical": "📍",
+		"venue-mixed": "📍",
+		"venue-unknown": "📍",
+	} as Record<SnippetIcon, string>)[snippet.icon] : "";
+
+	let label = snippet.label ? snippetLabelToMarkdown(snippet.label) : "";
+
+	return [emoji, label].filter(Boolean).join(" ");
 };
