7c3a263e27620d38ca3351a88eda21ede5dd992e
deniz <43997085+deniz-blue@users.noreply.github.com>
2026-03-23 20:37:49 +0200

    make use of ssg

--- apps/opener/vite.config.ts
+++ apps/opener/vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { readFileSync } from "node:fs";
+import { VirtualModule } from "@deniz-blue/vite-plugins";
 
 export default defineConfig({
 	plugins: [
 		react(),
-		{
-			name: "virtual-instances",
-			resolveId(id) {
-				if (id === "virtual:instances") return id;
-			},
-			async load(id) {
-				if (id !== "virtual:instances") return null;
+		VirtualModule(
+			"instances",
+			() => {
 				const json = JSON.parse(readFileSync("../../data/instances.json", "utf-8"));
 				return `export const instances = ${JSON.stringify(json)};`;
 			},
-		},
+		),
 	],
 })
